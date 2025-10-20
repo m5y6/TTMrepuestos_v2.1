7891a16b78e3576be20be8850037446f1a9c438b
m5y6 <ben.cerdap@duocuc.cl>
2025-10-19 21:39:49 -0300

    se arreglaron errores con las imagenes
    ahora toda la pagina web viene con sus respectivas imágenes

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -13,7 +13,7 @@ export default function Index() {
         <section className="segunda">
           <div id="galeria">
             <div className="contenedor-imagen">
-              <img className="imagen" src="img/volvo.png" alt="Camión Volvo en carretera" />
+              <img className="imagen" src="../../public/img/volvo.png" alt="Camión Volvo en carretera" />
               <h2 className="titulo">Truck & Trailer Melipilla</h2>
               <div className="texto">
                 <p>
@@ -32,42 +32,42 @@ export default function Index() {
             <h2>Productos más vendidos</h2>
             <div className="productos">
               <div className="producto">
-                <img src="img/bateria.png" alt="Batería de camión" />
+                <img src="../../public/img/bateria.png" alt="Batería de camión" />
                 <h3>Batería</h3>
                 <p>Batería de alto rendimiento para vehículos de carga pesada.</p>
                 <p className="precio">$100.000</p>
                 <button>Añadir al Carrito</button>
               </div>
               <div className="producto">
-                <img src="img/amortiguador.png" alt="Amortiguador de camión" />
+                <img src="../../public/img/amortiguador.png" alt="Amortiguador de camión" />
                 <h3>Amortiguador</h3>
                 <p>Amortiguador de alta resistencia para todo tipo de terreno.</p>
                 <p className="precio">$80.000</p>
                 <button>Añadir al Carrito</button>
               </div>
               <div className="producto">
-                <img src="img/filtro2.png" alt="Filtro de aceite para motor" />
+                <img src="../../public/img/filtro2.png" alt="Filtro de aceite para motor" />
                 <h3>Filtro de Aceite</h3>
                 <p>Filtro de aceite premium para motores diésel de alto cilindraje.</p>
                 <p className="precio">$20.000</p>
                 <button>Añadir al Carrito</button>
               </div>
               <div className="producto">
-                <img src="img/pastilla2.png" alt="Pastillas de freno para camión" />
+                <img src="../../public/img/pastilla2.png" alt="Pastillas de freno para camión" />
                 <h3>Pastillas de Freno</h3>
                 <p>Set de pastillas de freno cerámicas para máxima seguridad.</p>
                 <p className="precio">$50.000</p>
                 <button>Añadir al Carrito</button>
               </div>
               <div className="producto">
-                <img src="img/aceite2.png" alt="Bidón de aceite de motor" />
+                <img src="../../public/img/aceite2.png" alt="Bidón de aceite de motor" />
                 <h3>Aceite de Motor</h3>
                 <p>Aceite sintético 15W-40 para motores diésel modernos.</p>
                 <p className="precio">$30.000</p>
                 <button>Añadir al Carrito</button>
               </div>
               <div className="producto">
-                <img src="img/neumatico2.png" alt="Neumático de camión" />
+                <img src="../../public/img/neumatico2.png" alt="Neumático de camión" />
                 <h3>Neumático</h3>
                 <p>Neumático radial de larga duración para eje de tracción.</p>
                 <p className="precio">$200.000</p>
